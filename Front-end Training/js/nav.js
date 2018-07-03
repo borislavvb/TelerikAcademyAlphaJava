fa7bfa086120a79a766c0e34ffc91d42c3d78e56
Borislav Borisov <borislav.borisov32@gmail.com>
2018-07-03 11:07:02 +0300

    last changes without header

--- Front-end Training/js/nav.js
+++ Front-end Training/js/nav.js
@@ -1,14 +1,6 @@
-var nav = document.getElementById('nav');
-
-window.onscroll = function(){
+$('.menu-toggle').click(function() {
+  
+    $('.site-nav').toggleClass('site-nav--open', 500);
+    $(this).toggleClass('open');
     
-    if(window.pageYoffset > 100){
-        nav.style.background = white;
-        nav.style.boxShadow = "0px 4px 2px yellow";
-    }
-    else{
-        nav.style.background = "transparent";
-        nav.style.boxShadow = "none";
-
-    }
-}
+  })
